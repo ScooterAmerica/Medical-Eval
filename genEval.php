2b817bbd33c0234a11bf0d41d4959a9194ff35ab
Jeff Sciamanna <pyroguy6689@aim.com>
2012-03-07 17:27:07 -0500

    first of two general evaluation pages

---
+++ genEval.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Galloway Orthopedic Physical Therapy</title>
+<link href="file:///H|/SSE/genEvalCss.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<p align="center"><strong> <img src="file:///H|/SSE/general eval php form_clip_image002_0002.jpg" alt="" width="356" height="129" /></strong></p>
+<p align="center"><h1><strong><br/>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Robert Marisco Inc. <br/>
+  </strong>&nbsp;<strong>&nbsp;&nbsp;18 E. Jimmie Leads Rd. Suite B<br/>
+  </strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<strong>&nbsp; Galloway, NJ 08205 </strong><br/>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>P: (609) 652-3774 F: (609) 652-3776 </strong></h1></p>
+<br/>
+<p>
+  <strong>Name:  </strong></p>
+<p>
+  <label>Last</label><input name="Lastname" type="text" size="20" /> <label>First</label> <input name="Firstname" type="text" size="20" />
+  <label for="mid initial">MI</label>
+  <input type="text" name="mid initial" id="mid initial" size="1" maxlength="1"/>
+</p>
+<p><strong>Date:</strong></p>
+<p><input name="dateBox" type="text" id="dateBox" maxlength="10"/><label>mm/dd/yyyy</label>
+</p>
+<p><strong>DOB:</strong></p>
+<p>
+  <input name="dateBox" type="text" id="dateBox" maxlength="10" /><label>mm/dd/yyyy</label>
+</p>
+<p>&nbsp; </p>
+<p>
+  <label>SS#:</label>
+  <input name="SS number" type="text" align="middle" size="5" maxlength="3" /> 
+  -
+  <input name="ss number2" type="text" align="middle" size="4" maxlength="2" /> 
+  -
+  <input name="ss number3" type="text" align="middle" size="6" maxlength="4" />
+  
+</p>
+<p><br/>
+  <strong>Sex:</strong>
+  <label>
+    <input type="radio" name="gender" value="male" id="CheckboxGroup1_0" />
+    M</label>
+  <label>
+    <input type="radio" name="gender" value="female" id="CheckboxGroup1_1" />
+    F</label>
+  
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Hand Dominance:</strong>
+  <label>
+    <input type="radio" name="handdom" value="right" id="RadioGroup1_0" />
+    Right</label>
+  <label>
+    <input type="radio" name="handdom" value="left" id="RadioGroup1_1" />
+    Left</label> 
+</p>
+
+
+<p>
+  <strong> <label for="refer dr.">Referring Dr.</label></strong> 
+  <input name="refer dr." type="text" size="30" /></p>
+<p>&nbsp;</p>
+
+ 
+ <div>
+ <div align="center">
+ <p>
+	<textarea name="dxArea" cols="45" rows="3" id="dxArea"></textarea>
+	<textarea name="precautionArea" id="precautionArea" cols="45" rows="3"></textarea>
+</p>
+
+<p align="center"><strong> DX&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; Precautions </strong></p>
+
+<p> 
+  <textarea name="medField" id="medField" cols="30" rows="3"></textarea>
+  <textarea name="pmField" id="pmField" cols="30" rows="3"></textarea>
+</p>
+
+<p align="center"><strong> Meds &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; PM/SHx </strong></p>
+
+
+<p>
+  <textarea name="plofField" id="plofField" cols="30" rows="3"></textarea>
+  
+  </p>
+  
+  <p><strong> General Activity Level/POF</strong></p>
+</p>
+
+
+<p align="center">
+
+  <textarea name = "occupationField" id="occupationField" cols="30" rows="3"></textarea> 
+
+  <textarea name="socialField" id="socialField" cols="30" rows="3"></textarea> 
+</p>
+
+<p align="center"><strong> Occupation&nbsp;&nbsp;&nbsp;&nbsp; Social hx </strong></p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;</p>
+</div></div>
+ 
+ 
+<p><strong>Working:</strong> 
+  
+  <label>
+    <input type="checkbox" name="working" value="yes" id="CheckboxGroup2_2" />
+    Yes</label>
+  
+  <label>
+    <input type="checkbox" name="working" value="no" id="CheckboxGroup2_3" />
+    No</label>
+  
+  <label>
+    <input type="checkbox" name="working" value="ft" id="CheckboxGroup2_4" />
+    FT</label>
+  
+  <label>
+    <input type="checkbox" name="working" value="pt" id="CheckboxGroup2_5" />
+    PT</label>
+  
+  <label>
+    <input type="checkbox" name="working" value="light" id="CheckboxGroup2_6" />
+    Light</label>
+  <strong>
+  <label for="otherField">&nbsp;&nbsp;<br />
+    <br />
+  Other:</label>
+  <input type="text" name="otherField" id="otherField" />
+</strong></p>
+<p><br />
+</p>
+  <p><strong>Customs/religious beliefs or wishes that might affect care?</strong>
+  
+  <label>N/A</label></p> 
+<p><strong><label for="religionField"> If yes:</label></strong><input type="text" name="religionField" id="yes" />
+  </p>
+  </p>
+  <p>&nbsp;</p>
+<p><strong> Primary reason for Visit/Referrel:</strong></p>	
+<p>
+    <label for="visitArea"></label>
+<textarea name="visitArea" id="visitArea" cols="45" rows="7"></textarea>
+</p>
+<br/>
+<p><strong>Resting Vital Signs and Measures:</strong></p>
+
+
+
+<div>
+
+<div>
+<p align="left"> HR&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BP </p>
+
+
+<p><input name="heartRate" type="text" /><label>b/p/m</label>
+<input name="bpRate" type="text" /><label>mmHg</label>
+</p>
+</div>
+
+<div>
+<p> RR &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pulse O2%</p>
+<p><input name="respitoryRate" type="text" /><label>b/min</label>
+<input name="pulse" type="text" /><label>%</label>
+</p>
+</div>
+
+<div>
+<p align="left">Height&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Weight </p>
+
+<p><input name="height" type="text" /><label>ft/in</label>
+<input name="weight" type="text" /><label>lbs</label>
+</p>
+</div>
+
+<div>
+<p align="left">BF%&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Temp</p>
+
+<p>
+<input name="BF" type="text" /><label>%/lb.</label>
+<input name="Temp" type="text" /><label>°F</label>
+</p>
+</div>
+</div>
+
+
+<p><strong>Pain Level:(VAS) Overall</strong></p>
+<p>
+  <label>
+    <input type="radio" name="painlevel" value="0" id="RadioGroup2_0" />
+    0</label>
+  
+  <label>
+    <input type="radio" name="painlevel" value="1" id="RadioGroup2_1" />
+    1</label>
+  
+  <label>
+    <input type="radio" name="painlevel" value="2" id="RadioGroup2_2" />
+    2</label>
+  
+  <label>
+    <input type="radio" name="painlevel" value="3" id="RadioGroup2_3" />
+    3</label>
+  
+  <label>
+    <input type="radio" name="painlevel" value="4" id="RadioGroup2_4" />
+    4</label>
+  
+  <label>
+    <input type="radio" name="painlevel" value="5" id="RadioGroup2_5" />
+    5</label>
+  
+  <label>
+    <input type="radio" name="painlevel" value="6" id="RadioGroup2_6" />
+    6</label>
+  
+  <label>
+    <input type="radio" name="painlevel" value="7" id="RadioGroup2_7" />
+    7</label>
+    <label>
+    <input type="radio" name="painlevel" value="8" id="RadioGroup2_8" />
+    8</label>
+  
+  <label>
+    <input type="radio" name="painlevel" value="9" id="RadioGroup2_9" />
+    9</label>
+  
+  <label>
+    <input type="radio" name="painlevel" value="10" id="RadioGroup2_10" />
+    10</label>
+  <br />
+</p>
+<p>None----Annoying-----Marked-------Severe------911</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+<p><strong>Initial Onset Date of current pain:</strong> &nbsp;</p>
+<p><strong>Date:</strong></p>
+<p>
+   <input name="dateBox" type="text" maxlength="10" /><label>mm/dd/yyyy</label>
+</p>
+<p>&nbsp;</p>
+<p> 
+  <label for="causeField">Cause/MOI</label>
+  <input type="text" name="causeField" id="causeField" />
+
+&nbsp;&nbsp;&nbsp;Worst pain:<input name="worst" type="text" size="5"/>/10 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Least pain:<input name="" type="text" size="5" />/10</p>
+<p>Aggravating Factors:</p> 
+<p><textarea name="aggravate" cols="30" rows="5"></textarea></p>
+<p>Relieving Factors:</p>
+<p><textarea name="relief" cols="30" rows="5"></textarea></p>
+<p>&nbsp;</p>
+<p>
+  <label>
+    <input type="radio" name="factors" value="constant" id="RadioGroup3_0" />
+  Constant</label>
+  <label>
+    <input type="radio" name="factors" value="intermittent" id="RadioGroup3_1" />
+    Intermittent</label>&nbsp;&nbsp;<label>(Choose One)</label>
+  <br />
+</p>
+<p>
+  <label>
+    <input type="radio" name="factors1" value="localonly" id="RadioGroup4_0" />
+    Local Only</label>
+  <label>
+    <input type="radio" name="factors1" value="radicular" id="RadioGroup4_1" />
+    Radicular</label>&nbsp;&nbsp;<label>(Choose One)</label>
+  <br />
+</p>
+
+<p><strong>Description: (select all that apply)</strong></p>
+<p>
+  <label>
+    <input type="checkbox" name="description" value="sharp" id="CheckboxGroup3_0" />
+    Sharp</label>
+  <br/>
+  <label>
+    <input type="checkbox" name="description" value="dull" id="CheckboxGroup3_1" />
+    Dull</label>
+    <br/>
+    <label>
+    <input type="checkbox" name="description" value="ache" id="CheckboxGroup3_2" />
+    Ache</label>
+    <br/>
+   <label>
+    <input type="checkbox" name="description" value="deep" id="CheckboxGroup3_3" />
+    Deep</label>
+    <br/>
+   <label>
+    <input type="checkbox" name="description" value="superficialburn" id="CheckboxGroup3_4" />
+    Superficial Burn</label>
+<br/>
+  <label>
+    <input type="checkbox" name="description" value="pulsing" id="CheckboxGroup3_5" />
+    Pulsing</label>
+    <br/>
+  <label>
+    <input type="checkbox" name="description" value="throbbing" id="CheckboxGroup3_6" />
+    Throbbing</label>
+    <br/>
+  <label>
+    <input type="checkbox" name="description" value="tingling/numbness" id="CheckboxGroup3_7" />
+    Tingling/Numbness</label>
+  <br />
+</p>
+
+<p><strong>Joint Sounds:  (Select all that apply)</strong></p>
+<p>
+  <label>
+    <input type="checkbox" name="jointsounds" value="grating" id="CheckboxGroup4_0" />
+    Grating</label>
+    <br/>
+  <label>
+    <input type="checkbox" name="jointsounds" value="snapping" id="CheckboxGroup4_1" />
+    Snapping</label>
+    <br/>
+  <label>
+    <input type="checkbox" name="jointsounds" value="popping" id="CheckboxGroup4_2" />
+    Popping</label>
+    <br/>
+  <label>
+    <input type="checkbox" name="jointsounds" value="clicking" id="CheckboxGroup4_3" />
+    Clicking</label>
+  <br />
+</p>
+
+<p><strong>Patient Goals and Objectives:</strong></p>
+
+<p>
+  <textarea name="goalsField" cols="30" rows="5"></textarea>
+</p>
+<p>&nbsp;</p>
+<p>
+  <input type="submit" name="submitbtn" id="submitbtn" value="Save and Contiue" onclick="<a href = genEval2.php>" />
+</p>
+<br/>
+  
+  
+  
+<p> <a href="file:///H|/SSE/genEval2.php">Evaluation Page 2</a></p>
+</body>
+</html> 
---
+++ genEval2.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Galloway Orthopedic Physical Therapy Page 2</title>
+<link href="file:///H|/SSE/genEvalCss.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<p align="center"><strong><img src="file:///H|/SSE/general eval php form_clip_image002_0002.jpg" alt="" width="356" height="129" /></strong></p>
+
+<p align="center"><br/>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Robert Marisco Inc.</strong> <br/>
+  &nbsp;&nbsp;<strong>18 E. Jimmie Leads Rd. Suite B</strong><br/>
+  <strong>&nbsp;&nbsp;&nbsp; Galloway, NJ 08205</strong> <br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>P: (609) 652-3774 F: (609) 652-3776</strong> </p>
+
+<br/>
+
+<p><strong> Patient Name: <input name="patient name" type="text" />
+</strong>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong> Date: &nbsp;&nbsp;&nbsp;&nbsp;<input name="dateBox" type="text" id="dateBox" maxlength="10" />
+<label>mm/dd/yyyy</label>
+ </strong>
+ 
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong> ID#:<input name="idField" type="text" /></strong></p><br/>
+
+<div align="center">
+  <p align="center"><strong>Gait/Posture/Other Observations:</strong></p>
+  <p align="center"> 
+    <textarea name="postureField" cols="50" rows="5"></textarea>
+  </p>
+</div><br/>
+
+<div>
+<p align="center"><font size="4"><strong>Sensation/Reflexes &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Balance/Coordination</strong></font></p>
+
+<p align="center">
+<textarea name="sensationField" cols="30" rows="5"></textarea>
+<textarea name="balanceField" cols="30" rows="5"></textarea>
+</p>
+</div>
+<br/>
+
+
+<div align="center">
+  <p align="center"><strong>Strength&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ROM </strong></p>
+
+  <p align="center">
+    <textarea name="strengthField" cols="30" rows="5"></textarea>
+    
+<textarea name="romField" cols="30" rows="5"></textarea>
+</p>
+</div>
+
+<br/>
+
+<div>
+
+<p align="center">
+<font size="4"><strong>Special Tests&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Joint Mobs</strong></font></p>
+
+<p align="center">
+  <textarea name="textarea" id="textarea" cols="30" rows="5"></textarea>
+<textarea name="mobsFields" cols="30" rows="5"></textarea> 
+</p>
+</div>
+<br/>
+
+<p><strong> Functional Outcome Measures (Select) </strong></p>
+<p>
+  <label>
+    <input type="checkbox" name="CheckboxGroup1" value="checkbox" id="CheckboxGroup1_0" />
+    TUG</label>
+ 
+  <label>
+    <input type="checkbox" name="CheckboxGroup1" value="checkbox" id="CheckboxGroup1_1" />
+    Berg</label>
+ 
+  <label>
+    <input type="checkbox" name="CheckboxGroup1" value="checkbox" id="CheckboxGroup1_2" />
+    FR</label>
+ 
+  <label>
+    <input type="checkbox" name="CheckboxGroup1" value="checkbox" id="CheckboxGroup1_3" />
+    6MW</label>
+    
+&nbsp;&nbsp;&nbsp;&nbsp;Other: <input name="otherField" type="text" />    
+  </p>  
+  <br />
+  
+  <p><strong> Self Report Q's (Select) </strong></p>
+  <p>
+    <label>
+      <input type="checkbox" name="CheckboxGroup2" value="checkbox" id="CheckboxGroup2_0" />
+      BDI</label>
+    
+    <label>
+      <input type="checkbox" name="CheckboxGroup2" value="checkbox" id="CheckboxGroup2_1" />
+      NDI</label>
+    
+    <label>
+      <input type="checkbox" name="CheckboxGroup2" value="checkbox" id="CheckboxGroup2_2" />
+      DASH</label>
+    
+    <label>
+      <input type="checkbox" name="CheckboxGroup2" value="checkbox" id="CheckboxGroup2_3" />
+      LEFS</label>
+    
+    <label>
+      <input type="checkbox" name="CheckboxGroup2" value="checkbox" id="CheckboxGroup2_4" />
+      FABQ</label>
+   
+   Other: <input name="otherField" type="text" />
+</p>
+    <br />
+
+<p><strong>Addendums (Select)</strong> </p>
+<p>
+  <label>
+    <input type="checkbox" name="CheckboxGroup3" value="checkbox" id="CheckboxGroup3_0" />
+    Cervical</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup3" value="checkbox" id="CheckboxGroup3_1" />
+    Thoracic</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup3" value="checkbox" id="CheckboxGroup3_2" />
+    Lumbar</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup3" value="checkbox" id="CheckboxGroup3_3" />
+    Shoulder</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup3" value="checkbox" id="CheckboxGroup3_4" />
+    Elbow</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup3" value="checkbox" id="CheckboxGroup3_5" />
+    Wrist/Hand</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup3" value="checkbox" id="CheckboxGroup3_6" />
+    Wound</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup3" value="checkbox" id="CheckboxGroup3_7" />
+    Neuro</label>
+    
+    Other: <input name="otherField" type="text" />
+</p>
+  <br />
+
+<p><strong>Assessment: PT practice patterns: (</strong>
+
+
+  
+<label>
+      <input type="checkbox" name="CheckboxGroup4" value="checkbox" id="CheckboxGroup4_0" />
+      Cardiopulmonary</label>
+    
+    <label>
+      <input type="checkbox" name="CheckboxGroup4" value="checkbox" id="CheckboxGroup4_1" />
+      Neurologic</label>
+    
+    <label>
+      <input type="checkbox" name="CheckboxGroup4" value="checkbox" id="CheckboxGroup4_2" />
+      Musculoskeletal</label>
+    
+    <label>
+      <input type="checkbox" name="CheckboxGroup4" value="checkbox" id="CheckboxGroup4_3" />
+      Integumentary</label>)
+    <br />
+</p>
+
+<p><strong>Rehab Potential:</strong></p>
+<p>
+  <label>
+    <input type="radio" name="CheckboxGroup5" value="radio" id="CheckboxGroup5_0" />
+    Excellent</label>
+  
+  <label>
+    <input type="radio" name="CheckboxGroup5" value="radio" id="CheckboxGroup5_1" />
+    Good</label>
+  
+  <label>
+    <input type="radio" name="CheckboxGroup5" value="radio" id="CheckboxGroup5_2" />
+    Fair</label>
+  
+  <label>
+    <input type="radio" name="CheckboxGroup5" value="radio" id="CheckboxGroup5_3" />
+    Poor</label>
+  <br />
+</p>
+
+<div>
+
+<p align="left"> <strong>Goals (STG's)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Outcomes (LTG's)</strong></p>
+
+<p align="left"><textarea name="stgField" cols="30" rows="5"></textarea>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<textarea name="ltgField" cols="30" rows="5"></textarea></p>
+</div>
+<br/>
+
+<p><strong> Plan of Care:</strong></p>
+
+<p>
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_0" />
+    Therex</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_1" />
+    Gait Training</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_2" />
+    Theraputic Activity</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_3" />
+    US/phonophoresis</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_4" />
+    Balance/NRE</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_5" />
+    Manual Techniques</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_6" />
+    E-stim</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_7" />
+    Fitness/Wellness Program</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_8" />
+    Patient/Caregiver Education</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_9" />
+    Whirlpool</label>
+</p>
+<p>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_10" />
+    Biofeedback</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_11" />
+    Work Hardening/conditioning</label>
+  
+  <label>
+    <input type="checkbox" name="CheckboxGroup6" value="checkbox" id="CheckboxGroup6_12" />
+    Thermal agents(hot/cold packs)</label>
+  
+  Other: <input name="otherField" type="text" />
+</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+<p> <strong>Robert Marsico, PT, Ed.D. NJ lic. # 40QA0842100</strong></p>
+  	 
+<p><strong>Therapist Signature: <input name="sigField" type="text" /></strong></p>		
+<p><strong>Date:</strong>
+  <input name="dateField" type="text" />
+</p>
+<p>&nbsp;</p>
+<p>&nbsp; </p>
+<p><strong>Physician Certification: (for Medicare patients only)  As per Medicare guidelines, please review the above plan of care, and upon your approval sign and date below.  Thank you for your cooperation.</strong></p>
+
+<p><strong>Physician Signature:<input name="physSigField" type="text" /></strong></p>
+<p><strong>Date:</strong>
+  <input name="dateField" type="text" />
+
+</p>
+
+<p>&nbsp;</p>
+<p>
+  <input type="submit" name="submitbtn2" id="submitbtn2" value="Save and Continue" />
+</p>
+</body>
+</html>
